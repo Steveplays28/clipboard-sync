873e7aae65b82ab35d25262203af2d5480f3e7af
Darion Spaargaren <djspaargaren@outlook.com>
2022-02-26 16:16:43 +0100

    Use 4 width tabs >:)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import React, { useState } from 'react'
-import { invoke } from '@tauri-apps/api/tauri'
+import React, { useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
 
-import './App.css'
+import './App.css';
 
 function App() {
-  const [msgFromRust, setMsgFromRust] = useState('')
-  const [inputValue, setInputValue] = useState('')
+	const [msgFromRust, setMsgFromRust] = useState('');
+	const [inputValue, setInputValue] = useState('');
 
-  const handleHelloWorld = async () => {
-    try {
-      const response = await invoke('hello_world_test', {
-        event: inputValue || 'nope',
-      })
-      setMsgFromRust(`${response}`)
-      console.log('response ', response)
-    } catch (error) {
-      console.log('error ', error)
-    }
-  }
+	const handleHelloWorld = async () => {
+		try {
+			const response = await invoke('hello_world_test', {
+				event: inputValue || 'nope',
+			});
+			setMsgFromRust(`${response}`);
+			console.log('response ', response);
+		} catch (error) {
+			console.log('error ', error);
+		}
+	};
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <div className='component-wrapper'>
-          <input
-            value={inputValue}
-            placeholder="input for rust"
-            onChange={(e) =>
-              setInputValue(e.target.value)
-            }
-          />
-          <button onClick={handleHelloWorld}>call rust</button>
-          {!!msgFromRust && (
-            <p style={{ position: 'absolute' }}>
-              response message: {msgFromRust}
-            </p>
-          )}
-        </div>
-      </header>
-    </div>
-  )
+	const components = ['div', 'div', 'div'];
+	return (
+		<div>
+			{components.map((comp, i) => React.createElement(comp, { key: i }))}
+		</div>
+	);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div className='component-wrapper'>
+					<input
+						value={inputValue}
+						placeholder='input for rust'
+						onChange={(e) => setInputValue(e.target.value)}
+					/>
+					<button onClick={handleHelloWorld}>call rust</button>
+					{!!msgFromRust && (
+						<p style={{ position: 'absolute' }}>
+							response message: {msgFromRust}
+						</p>
+					)}
+				</div>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app'),
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('app')
 );
